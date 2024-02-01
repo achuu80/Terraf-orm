236940c3c5cbdb92c7eda7c81c09600ba675071c
achuu80 <archanaachuu80@gmail.com>
2024-02-01 19:22:41 +0000

    Upload vnetsubnet

--- providers.tf
+++ providers.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 
-# Configure the Microsoft Azure Provider
+# Configure the Microsoft Azure Provider - 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
 provider "azurerm" {
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
